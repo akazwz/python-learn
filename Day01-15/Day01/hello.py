a0d14af0879db3664571143f9ec68ac61b41fd8e
akez <1791189210@qq.com>
2021-04-23 20:26:35 +0800

    Initial commit

---
+++ Day01-15/Day01/hello.py
@@ -0,0 +1,12 @@
+def hello_world():
+    print('Hello, World!')
+    print('Hello', 'World', sep=', ', end='!\n')
+    print('Goodbye, World', end='!\n')
+
+
+def main():
+    hello_world()
+
+
+if __name__ == '__main__':
+    main()
